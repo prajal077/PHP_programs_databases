1e5d37d861e9a3a5ce1dcfe4cc5443a1c7aa64a5
Prajal <prajalbhattarai1627@gmail.com>
2025-04-25 22:59:27 +0545

    table created successfully

--- table.php
+++ table.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli ("localhost", "root", "");
+$conn = new mysqli ("localhost", "root", "", "student_db");
 if($conn -> connect_error) {
     die("Connection error".$conn -> connect_error);
 }
@@ -7,7 +7,16 @@ if($conn -> connect_error) {
 echo "Connection successfull";
 echo "<br>";
 
-$sql = "CREATE TABLE student"
+$sql = "CREATE TABLE std (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), email VARCHAR(20) UNIQUE)" ;
+
+if($conn -> query($sql) == TRUE) {
+    echo "table created successfully";
+}
+else {
+    echo "Table is not created", $conn -> error ;
+}
+
+$conn -> close() ;
 
 
 ?>
