e51a272ec31ddfa34ea4c387150fccd08cb2dadd
Prajal <prajalbhattarai1627@gmail.com>
2025-04-25 23:45:19 +0545

    select.php in table format

---
+++ select.php
@@ -0,0 +1,51 @@
+<?php
+$conn = new mysqli ("localhost", "root", "", "student_db");
+if($conn -> connect_error) {
+    die("Connection error".$conn -> connect_error);
+}
+
+echo "Connection successfull";
+echo "<br>";
+
+$sql = "SELECT id, name, email FROM std where id = 4 ";
+
+$res = $conn -> query($sql);
+//print_r($res > fetch_assoc());
+
+
+// if($res -> num_rows > 0) {
+//     while($row=$res -> fetch_assoc()) {
+//         echo $row['id']."<br>";
+//         echo $row['name']."<br>";
+//         echo $row['email']."<br>";
+//     }
+// }
+
+
+
+//         <-- IN TABLE FORMAT -->
+
+if ($res->num_rows > 0) {
+    // Output in table format
+    echo "<table border='1' cellpadding='5' cellspacing='0'>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>";
+
+    while ($row = $res->fetch_assoc()) {
+        echo "<tr>
+                <td>{$row['id']}</td>
+                <td>{$row['name']}</td>
+                <td>{$row['email']}</td>
+              </tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No records found.";
+}
+
+$conn->close();
+?>
