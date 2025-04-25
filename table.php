6ed0ffc0c06aeb6225cbca53821ee5440def664c
Prajal <prajalbhattarai1627@gmail.com>
2025-04-25 22:42:44 +0545

    creating table

---
+++ table.php
@@ -0,0 +1,13 @@
+<?php
+$conn = new mysqli ("localhost", "root", "");
+if($conn -> connect_error) {
+    die("Connection error".$conn -> connect_error);
+}
+
+echo "Connection successfull";
+echo "<br>";
+
+$sql = "CREATE TABLE student"
+
+
+?>
