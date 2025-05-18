920358d179081316bab0f7e26cc672af831661f4
Prajal <prajalbhattarai1627@gmail.com>
2025-05-18 07:34:29 +0545

    used another method foreach instead of while

--- select.php
+++ select.php
@@ -13,39 +13,39 @@ $res = $conn -> query($sql);
 //print_r($res > fetch_assoc());
 
 
-// if($res -> num_rows > 0) {
-//     while($row=$res -> fetch_assoc()) {
-//         echo $row['id']."<br>";
-//         echo $row['name']."<br>";
-//         echo $row['email']."<br>";
-//     }
-// }
+if ($res->num_rows > 0) {
+    foreach ($res as $row) {
+        echo "ID: {$row['id']} - Name: {$row['name']} - Email: {$row['email']}<br>";
+    }
+} else {
+    echo "0 results";
+}
 
 
 
 //         <-- IN TABLE FORMAT -->
 
-if ($res->num_rows > 0) {
-    // Output in table format
-    echo "<table border='1' cellpadding='5' cellspacing='0'>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-            </tr>";
+// if ($res->num_rows > 0) {
+//     // Output in table format
+//     echo "<table border='1' cellpadding='5' cellspacing='0'>
+//             <tr>
+//                 <th>ID</th>
+//                 <th>Name</th>
+//                 <th>Email</th>
+//             </tr>";
 
-    while ($row = $res->fetch_assoc()) {
-        echo "<tr>
-                <td>{$row['id']}</td>
-                <td>{$row['name']}</td>
-                <td>{$row['email']}</td>
-              </tr>";
-    }
+//     while ($row = $res->fetch_assoc()) {
+//         echo "<tr>
+//                 <td>{$row['id']}</td>
+//                 <td>{$row['name']}</td>
+//                 <td>{$row['email']}</td>
+//               </tr>";
+//     }
 
-    echo "</table>";
-} else {
-    echo "No records found.";
-}
+//     echo "</table>";
+// } else {
+//     echo "No records found.";
+// }
 
 $conn->close();
 ?>
